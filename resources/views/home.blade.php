d9054c0b16908532871c667332fc2b9877171284
Your Name <jxtsly@gmail.com>
2024-01-09 19:02:57 +0100

    dashboard changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,10 +91,14 @@ https://templatemo.com/tm-558-klassy-cafe
                                     @auth
                                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                                     @else
-                                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                                        <li>
+                                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                                            </li>
                 
                                         @if (Route::has('register'))
-                                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                            <li>
+                                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                                </li>
                                         @endif
                                     @endauth
                                 </div>
