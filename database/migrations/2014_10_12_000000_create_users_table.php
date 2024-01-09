cd94c2e88bd6f3a0b263d8985a21980e9391924a
Your Name <jxtsly@gmail.com>
2024-01-09 18:45:15 +0100

    usertype

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique(); 
+            $table->string('usertype')->default(0); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
