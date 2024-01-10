252a926225f405bce9d7a67943a489707d9035d9
Your Name <jxtsly@gmail.com>
2024-01-10 09:56:58 +0100

    usertype function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        return view('home'); 
+    }
+
+    public function redirects(){
+        $usertype = Auth::user()->usertype();
+        if($usertype =="1"){
+            return view();
+        }
+        else{
+            return view('home');
+        }
     }
 }
