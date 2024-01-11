b81d97c8def87caa3b271285a7a486de9e7e2810
Your Name <jxtsly@gmail.com>
2024-01-11 13:13:15 +0100

    users

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get("/", [HomeController::class,"index"]);
+
+Route::get("/users", [AdminController::class,"user"]);
 
 Route::get("/redirects", [HomeController::class,"redirects"]);
 
