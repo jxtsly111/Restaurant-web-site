8989d5bc23cf6bbf6edbf0fb283eaf918290b7fe
Your Name <jxtsly@gmail.com>
2024-01-12 09:00:46 +0100

    foodmenu

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,4 +18,9 @@ class AdminController extends Controller
         $data->delete();
         return redirect()->back();
     }
+
+    public function foodmenu(){
+       
+        return view('admin.foodmenu');
+    }
 }
