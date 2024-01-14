db9fb0532047b4de6b9dbdff135fd1fc30df59d6
Your Name <jxtsly@gmail.com>
2024-01-14 09:14:50 +0100

    form view

--- resources/views/admin/updateview.blade.php
+++ resources/views/admin/updateview.blade.php
@@ -13,6 +13,35 @@
 
    @include("admin.navbar")
 
+   <div style="position:relative; top:60px; right: -150px">
+    <form action="{{url('/uploadfood')}}" method="post" enctype="multipart/form-data">@csrf
+        <div>
+            <label>Title</label>
+            <input style="color: blue" type="text" name="title" value="{{$data->title}}" required>
+        </div>
+
+        <div>
+            <label>Price</label>
+            <input style="color: blue" type="number" name="price" value="{{$data->price}}" required>
+        </div>
+
+        <div>
+            <label>Image</label>
+            <input type="file" name="image" required>
+        </div>
+
+        <div>
+            <label>Description</label>
+            <input style="color: blue" type="text" name="description" value="{{$data->description}}" required>
+        </div>
+
+        <div>
+            <input style="color: black; background-color:white;" type="submit" value="Save">
+        </div>
+    </form>
+
+    <div>
+
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
