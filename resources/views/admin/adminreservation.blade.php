7477383c7a07ac25c12c4990f307242b6bd344ce
Your Name <jxtsly@gmail.com>
2024-01-15 09:22:46 +0100

    display reservation

--- resources/views/admin/adminreservation.blade.php
+++ resources/views/admin/adminreservation.blade.php
@@ -11,6 +11,31 @@
 
    @include("admin.navbar")
 
+   <div style="position: relative; top: 70px; right: -150;">
+    <table bgcolor = "grey" border="1px">
+        <tr>
+            <th style="padding: 30px">Name</th>
+            <th style="padding: 30px">Email</th>
+            <th style="padding: 30px">Phone</th>
+            <th style="padding: 30px">Date</th>
+            <th style="padding: 30px">Time</th>
+            <th style="padding: 30px">Message</th>
+        </tr>
+
+        @foreach($data as $data)
+        <tr align="center">
+            <td>{{$data->name}}</td>
+            <td>{{$data->email}}</td>
+            <td>{{$data->phone}}</td>
+            <td>{{$data->date}}</td>
+            <td>{{$data->time}}</td>
+            <td>{{$data->message}}</td>
+        </tr>
+
+        @endforeach
+    </table>
+   </div>
+
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
