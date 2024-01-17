4ce0a9c9449a9ec092d5f77d2475de3e933bbaf9
Your Name <jxtsly@gmail.com>
2024-01-17 09:10:03 +0100

    adminchef ui

--- resources/views/admin/adminchef.blade.php
+++ resources/views/admin/adminchef.blade.php
@@ -11,6 +11,36 @@
 
    @include("admin.navbar")
 
+
+   <form action="">
+
+    <div>
+
+      
+            <label>Name</label>
+            <input style="color: blue;" type="text" name="name" placeholder="Enter name" required>  
+    
+        </div>
+
+    <div>
+
+            <label>Speciality</label>
+            <input style="color: blue;" type="text" name="speciality" placeholder="Enter the speciality" required>
+    </div>
+
+    <div>
+
+        <input  type="file" name="image" required>
+</div>
+
+<div>
+
+   
+    <input  type="submit" value="save" required>
+</div>
+
+   </form>
+
    <h1>Admin Chef</h1>
 
     </div>
